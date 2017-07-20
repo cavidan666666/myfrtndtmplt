cec37be396beb03a8576a6fb0b9eaffb96de54c1
Cavidan <acavidan.p102@code.edu.az>
2017-07-20 11:52:37 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-    showSlides(slideIndex +=n);
+var index = 1;
+
+function plusIndex(n) {
+
+    index = index + 1;
+    showImage(index);
+    console.log(plusIndex)
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+showImage(1);
+
+function showImage(n) {
+
+    var x = document.getElementsByClassName("slides");
+    if (n>x.length) {index=1}
+    	if (n<1) {index=x.length}
+    for (var i = 0; i < x.length; i++) {
+
+        x[i].style.display = "none";
+    }
+    x[index-1].style.display = "block";
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName('mySlides');
-    var dots = document.getElementsByClassName ('dot');
 
-    if (n >slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1){
-        slideIndex=slides.length
-    }
-    for (i=0; i <slides.length ; i++){
-        slides[i].style.display= "none";
-    }
-    for (i = 0; i < dots.length; i++){
-        dots[i].className= dots[i].className.replace("active","");
 
-    }
-    slides[slideIndex-1].style.display = "block";
-}
+
+
+
